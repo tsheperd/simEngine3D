8f3d8e52648522c86079f0ff4db710ff6ced007d
Travis Sheperd <tsheperd@wisc.edu>
2020-12-12 05:14:19 -0600

    Compound Constraints Final
    The finalized compound constraint implementation

--- FinalProject/simEngine3D_A8P1.m
+++ FinalProject/simEngine3D_A8P1.m
@@ -48,13 +48,15 @@ simulation = simEngine3D;
 
 % Read the input deck
 %simulation.ReadInputDeck("revJoint.mdl");
+%simulation.ReadInputDeck("revJoint_2.mdl");
+
 %simulation.ReadInputDeck("revJoint2.mdl");
-%simulation.ReadInputDeck("revJoint4.mdl");
-simulation.ReadInputDeck("revJoint_2.mdl");
+simulation.ReadInputDeck("revJoint4.mdl");
+
 
 % Run the kinematic solver: (t_initial, dt, t_final, tolerance)
 %simulation.InverseDynamicsSolver(0, 0.01, 10, 1e-6);
-simulation.DynamicsSolver(0, 0.05, 10, 1e-4);
+simulation.DynamicsSolver(0, 0.005, 10, 1e-4);
 
 
 
